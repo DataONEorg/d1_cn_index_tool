26222ca98b99ff55c48e05c02f2d83faeb603257
Sky Roseboom <sroseboo@epscor.unm.edu>
2011-12-09 14:55:40 +0000

    Initial commit - tool to build/refresh solr index from cn systemMetadata map

---
+++ src/main/java/org/dataone/cn/utility/SolrIndexBuildTool.java
@@ -0,0 +1,105 @@
+package org.dataone.cn.utility;
+
+import org.apache.log4j.Logger;
+import org.dataone.cn.hazelcast.HazelcastClientInstance;
+import org.dataone.cn.index.generator.IndexTaskGenerator;
+import org.dataone.cn.index.processor.IndexTaskProcessor;
+import org.dataone.configuration.Settings;
+import org.dataone.service.types.v1.Identifier;
+import org.dataone.service.types.v1.SystemMetadata;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.IMap;
+
+/**
+ * This test currently relies upon: a postgres install and config see:
+ * cn_index_common, and a solr server to connect to, see: solr.properties.
+ * 
+ * @author sroseboo
+ * 
+ */
+public class SolrIndexBuildTool {
+    private static Logger logger = Logger.getLogger(SolrIndexBuildTool.class.getName());
+
+    private static final String HZ_SYSTEM_METADATA = Settings.getConfiguration().getString(
+            "dataone.hazelcast.systemMetadata");
+
+    private static final String HZ_OBJECT_PATH = Settings.getConfiguration().getString(
+            "dataone.hazelcast.objectPath");
+
+    private HazelcastClient hzClient;
+    private IMap<Identifier, SystemMetadata> systemMetadata;
+    private IMap<Identifier, String> objectPaths;
+    private ApplicationContext context;
+
+    private IndexTaskGenerator generator;
+    private IndexTaskProcessor processor;
+
+    public SolrIndexBuildTool() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting solr index refresh.");
+        SolrIndexBuildTool indexTool = new SolrIndexBuildTool();
+
+        try {
+            refreshSolrIndex(indexTool);
+        } catch (Exception e) {
+            System.out.println("Solr index refresh failed: " + e.getMessage());
+            e.printStackTrace(System.out);
+        }
+
+        System.out.println("Exiting solr index refresh tool.");
+    }
+
+    private static void refreshSolrIndex(SolrIndexBuildTool indexTool) {
+        indexTool.configureHazelcast();
+        indexTool.configureContext();
+        indexTool.generateIndexTasksAndProcess();
+        // call processor a final time to process resource maps that could not
+        // process on first pass (references not indexed delays resource map
+        // indexing)
+        indexTool.processIndexTasks();
+    }
+
+    private void generateIndexTasksAndProcess() {
+        int count = 0;
+        for (SystemMetadata smd : systemMetadata.values()) {
+            String objectPath = retrieveObjectPath(smd.getIdentifier().getValue());
+            generator.processSystemMetaDataUpdate(smd, objectPath);
+            count++;
+            if (count > 1000) {
+                processIndexTasks();
+                count = 0;
+            }
+        }
+        // call processor:
+        // it won't be called on last iteration of for loop if count < 1000
+        processIndexTasks();
+    }
+
+    private void processIndexTasks() {
+        processor.processIndexTaskQueue();
+    }
+
+    private String retrieveObjectPath(String pid) {
+        Identifier PID = new Identifier();
+        PID.setValue(pid);
+        return objectPaths.get(PID);
+    }
+
+    private void configureHazelcast() {
+        logger.info("starting hazelcast client...");
+        hzClient = HazelcastClientInstance.getHazelcastClient();
+        systemMetadata = hzClient.getMap(HZ_SYSTEM_METADATA);
+        objectPaths = hzClient.getMap(HZ_OBJECT_PATH);
+    }
+
+    private void configureContext() {
+        context = new ClassPathXmlApplicationContext("index-tool-context.xml");
+        generator = (IndexTaskGenerator) context.getBean("indexTaskGenerator");
+        processor = (IndexTaskProcessor) context.getBean("indexTaskProcessor");
+    }
+}
